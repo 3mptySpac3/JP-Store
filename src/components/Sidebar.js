069727dda3aeb143ce49ead3ee554955db8236af
3mptySpac3 <thefantasticalproject@gmail.com>
2024-03-21 02:31:47 -0600

    fix

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -46,7 +46,7 @@ const Sidebar = () => {
 
   return (
   <div ref={sidebarRef} className={`${isOpen ? 'right-0' : '-right-full'} w-full bg-[white] fixed top-0 h-full shadow-2xl md:w-[35vw] xl:max-w-[30vw]
-  transition-all duration-300 z-20 px-4 lg:px-[35px]`}>
+  transition-all duration-300 z-20 px-4 lg:px-[35px]  max-h-screen flex flex-col justify-between overflow-hidden`}>
     <div className='flex items-center justify-between py-6 border-b border-[black]'>
       <div className='uppercase text-sm font-semibold'>Shopping Bag ({itemAmount})</div>
       {/* icon */}
@@ -69,7 +69,7 @@ const Sidebar = () => {
         {/* Clear cart */}
         <div onClick={clearCart} className='cursor-pointer py-4 rounded-lg bg-black text-white w-12 h-12 flex justify-center items-center text-xl
         hover:scale-90 transition-transform duration-200'>
-          <FiTrash2 className=''/>
+          <FiTrash2/>
         </div>
       </div>
       <div className='flex justify-start items-center gap-4'> {/* Add gap for spacing */}
